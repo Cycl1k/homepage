84f664642c86a12a32455ccd372005429f10c5f2
Cycl1k <cycl1k@outlook.com>
2023-12-12 15:56:36 +0500

    modified:   static/js/reload_nodes.js 	modified:   templates/proxmox.html

--- static/js/reload_nodes.js
+++ static/js/reload_nodes.js
@@ -4,7 +4,13 @@ setInterval(function(){$.ajax({
     success: function(response) {
         for (let i=0; i<=response[-1]; i++){
             console.log('time' + i, response[i]["node_uptime"]);
-            $("#time"+i).html(response[i]["node_uptime"]); 
+            $("#node_uptime"+i).html(response[i]["node_uptime"]); 
+            $("#node_core_load"+i).html(response[i]["node_core_load"]); 
+            $("#node_core"+i).html(response[i]["node_core"]); 
+            $("#node_ram_moment"+i).html(response[i]["mem"]); 
+            $("#node_os_disk_total"+i).html(response[i]["node_os_disk_total"]); 
+            $("#node_os_disk_moment"+i).html(response[i]["node_os_disk_moment"]); 
+            $("#node_status"+i).html(response[i]["node_status"]); 
         }
     },
     error: function(error) {
